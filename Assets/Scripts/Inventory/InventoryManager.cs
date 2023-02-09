1ad51f6cd9dd1b5f6c73454687235a84ebc809ec
Archeronie <123574916+Archeronie@users.noreply.github.com>
2023-02-09 17:06:53 -0500

    Revert "Merge branch 'main' of https://github.com/JunoStrunk/GameContent2_DIG3715_Group"
    This reverts commit f2062bc2276058eee1fc4598c01f3cacbb902d7e, reversing
    changes made to 9ac927126fca7c4311d859215cf37dd4516e43df.

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public List<InventoryItem> InventoryList = new List<InventoryItem>();
-    GameObject inventorySlots;
 
     InventoryItem activeItem = null;
     int activeItemIndex = 0;
 
     private void Start() //Important to listen only on start or else there will be a null reference for singleton
     {
-        inventorySlots = GameObject.Find("Slots");
-        
         GameEventSys.current.onItemPickUp += AddItem;
     }
 
